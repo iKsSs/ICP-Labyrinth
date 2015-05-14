9f07f11379c1b2455763a4b4831be43218320898
iksss <iksss@1af6c692-271a-4d53-9805-3be62cbccae9>
2015-05-14 14:02:38 +0000

    CLI

---
+++ projekt/main-cli.cpp
@@ -0,0 +1,21 @@
+//ICP-Labyrinth 2014/2015
+/**
+* @file       main-cli.cpp
+* @author     Jakub Pastuszek (xpastu00@stud.fit.vutr.cz)
+* @date       May 2015
+*
+* CLI application
+*/
+
+#include "board.h"
+
+#include <QTextStream>
+
+
+
+int main(int argc, char* argv[]){
+	using namespace std;
+
+	QTextStream(stdout) << "NECO" << endl;
+
+}
